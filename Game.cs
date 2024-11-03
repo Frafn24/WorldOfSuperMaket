23813d615342a4ed5cdcaaa21fd1b8be69f46caf
Frafn24 <frafn24@student.sdu.dk>
2024-11-03 11:50:04 +0100

    Add project files.

---
+++ Game.cs
@@ -0,0 +1,44 @@
+/* Main class for launching the game
+ */
+
+class Game {
+  static World    world    = new World();
+  static Context  context  = new Context(world.GetEntry());
+  static ICommand fallback = new CommandUnknown();
+  static Registry registry = new Registry(context, fallback);
+  
+  private static void InitRegistry () {
+    ICommand cmdExit = new CommandExit();
+    registry.Register("exit", cmdExit);
+    registry.Register("quit", cmdExit);
+    registry.Register("bye", cmdExit);
+    registry.Register("go", new CommandGo());
+    registry.Register("help", new CommandHelp(registry));
+  }
+
+    public void run()
+    {
+        Console.WriteLine("Welcome to the World of Zuul!");
+
+
+        InitRegistry();
+        context.GetCurrent().Welcome();
+
+        while (context.IsDone() == false)
+        {
+            Console.Write("> ");
+            var line = Console.ReadLine();
+            if (line != null)
+            {
+                registry.Dispatch(line);
+            }
+            if (string.IsNullOrEmpty(line))
+            {
+                context.MakeDone();
+            }
+        }
+        Console.WriteLine("Game Over 😥");
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        RunGame();
+        Console.ReadLine();
+
+    }
+    static void RunGame()
+    {
+        Game game = new Game();
+        game.run();
+    }
+}
