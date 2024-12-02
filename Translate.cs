325b18c2ee1df042f673dec2093b136196c61459
FrederikThy <frederik151100@gmail.com>
2024-12-02 16:16:42 +0100

    Kommentering af Translate.cs

--- Translate.cs
+++ Translate.cs
@@ -1,16 +1,23 @@
 namespace WorldOfSuperMaket;
 
-internal class Translate
+public class Translate
 {
     // Variable
     private string filename = "/Users/frederikthygesen/Downloads";
+    // Dictionary der gemmer oversættelser
     private Dictionary<string, string> ActiveTranslations = new Dictionary<string, string>();
 
+    // Indlæser oversættelserne
     public void SetLanguage(string language)
     {
+        // .Skip(1) skipper den første linje
+        // .Select(ParseCsvLine) parser de forskellige parametre, så det er læsbart
+        // .ToList() konverterer det til en liste
         var translations = File.ReadAllLines(GetPlacement("Translation.csv")).Skip(1).Select(ParseCsvLine).ToList();
 
+        // Tømmer dictionary når vi skifter sprog, så der ikke sker fejl.
         ActiveTranslations.Clear();
+        
         // tjekker hvilket sporg der ønskes, og vælger det i csv filen.
         foreach (var translation in translations)
         {
@@ -30,6 +37,7 @@ internal class Translate
 
         string GetTranslations(string key)
         {
+            // Tjekker om key'en findes i vores dictionary "ActiveTranslations"
             if (!ActiveTranslations.ContainsKey(key))
             {
                 return ActiveTranslations[key];
@@ -39,9 +47,14 @@ internal class Translate
                 return $"Missing translation for {key}";
             }
         }
-
+        // En metode der kan sikrer sig, at den rigtige sti bliver kaldt uanset
+        // operativsystem. 
+        // Måske vi skal gøre det på samme måde som de andre CSV-filer?
         string GetPlacement(string Filename) => Path.Combine(filename, Filename);
 
+        // Deler filen op efter hvor der er et komma. 
+        // Den gør det muligt for os at lave metoden "TranslationData" og
+        // Constructoren "TranslationData".
         TranslationData ParseCsvLine(string line)
         {
             var parts = line.Split(',');
@@ -49,6 +62,7 @@ internal class Translate
         }
     }
 
+    // Metode der instansierer vores variable, og laver en constructor. 
     public class TranslationData
     {
         public string Key { get; set; }
@@ -63,3 +77,23 @@ internal class Translate
         }
     }
 }
+
+// Eksempel på implementering i koden:
+
+/*
+Hvis vi kigger på inventoryCommand har vi en Console.WriteLine
+som printer "Vis varer i dit inventory." Hvis vi gerne vil have den
+til, at kunne oversætte sig selv baseret på det valgte sprog, kan
+vi skrive det på følgende måde:
+
+Console.WriteLine(translator.GetTranslation("key: show_items_in_inv"), command);
+
+I dette tilfælde behøver vi faktisk ikke skrive "command" til sidst, fordi der er
+ikke noget, som brugeren selv kan have skrevet ind. Hvis vi kigger på en anden 
+Console.WriteLine som fx "Woopsie, jeg forstår ikke {command}"
+ville command kunne hente kommandoen fra csv-filen
+
+Grunden til vi kan gøre det er fordi, vi starter med at istantisere:
+
+private Translate translator;
+*/
